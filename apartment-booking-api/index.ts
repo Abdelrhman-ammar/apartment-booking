29617197ff86cbf078328a8c19e084f24d2031d4
Abdelrhman Ammar <abdelrhmanammar.cs@gmail.com>
2025-04-22 21:09:04 +0200

    Feat: setup typescript

--- apartment-booking-api/index.ts
+++ apartment-booking-api/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello from Apartment Booking Api 2");
+});
+
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
 }); 
